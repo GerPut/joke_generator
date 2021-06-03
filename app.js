2be9ba2e129e45f0336a72b745b707bba441691c
Gerhard <gerhard.putter@yahoo.com>
2021-06-03 13:32:20 +0200

    Created new elements with their styles.

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 const body = document.body.style.background = "green"
+
+
+function addNode() {
+    //Variables
+    let divNode = document.createElement("div");
+    let paragraphNode = document.createElement("p");
+    let textNode = document.createTextNode("hello");
+    let button = document.createElement("button")
+
+    //Styles
+    divNode.style.backgroundColor = "red";
+    divNode.style.height = "400px";
+    divNode.style.width = "700px";
+    divNode.style.marginTop = "200px"
+
+    paragraphNode.style.color = "white"
+    paragraphNode.style.fontFamily = "Varela Round, sans-serif"
+    paragraphNode.style.fontSize = "2rem"
+    paragraphNode.style.textAlign = "center"
+    paragraphNode.style.margin = "100px"
+
+    button.style.height = "100px";
+    button.style.width = "300px";
+    button.style.marginTop = "20px"
+    button.style.fontFamily = "Varela Round, sans-serif";
+    button.style.textAlign = "center";
+    button.innerText = "Press for a Joke"
+
+
+    //Insert
+    document.body.appendChild(divNode);
+    document.body.appendChild(button)
+    paragraphNode.appendChild(textNode);
+    divNode.appendChild(paragraphNode);
+
+
+    //Get Jokes Function
+
+
+
+}
+
+addNode();
