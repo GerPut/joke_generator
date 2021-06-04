d24c0cfe239b9919f3d9a9eb907aca70d1577217
Gerhard <gerhard.putter@yahoo.com>
2021-06-04 09:17:34 +0200

    Added async/await function to fetch jokes api.

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-const body = document.body.style.background = "green"
+const body = document.body.style.background = "#F65158"
 
 
 function addNode() {
     //Variables
     let divNode = document.createElement("div");
     let paragraphNode = document.createElement("p");
-    let textNode = document.createTextNode("hello");
+    let textNode = document.createElement("div");
     let button = document.createElement("button")
 
     //Styles
-    divNode.style.backgroundColor = "red";
+    divNode.style.backgroundColor = "#FBDE45";
     divNode.style.height = "400px";
     divNode.style.width = "700px";
+    divNode.style.border = "2px solid #28324B"
     divNode.style.marginTop = "200px"
 
-    paragraphNode.style.color = "white"
+    paragraphNode.style.color = "#28324B"
     paragraphNode.style.fontFamily = "Varela Round, sans-serif"
     paragraphNode.style.fontSize = "2rem"
     paragraphNode.style.textAlign = "center"
-    paragraphNode.style.margin = "100px"
+    paragraphNode.style.margin = "80px"
 
     button.style.height = "100px";
     button.style.width = "300px";
     button.style.marginTop = "20px"
     button.style.fontFamily = "Varela Round, sans-serif";
     button.style.textAlign = "center";
-    button.innerText = "Press for a Joke"
+    button.style.borderRadius = "100px"
+    button.style.border = " 2px solid #28324B"
+    button.innerText = "Click me for a Joke"
 
 
     //Insert
@@ -36,7 +39,15 @@ function addNode() {
 
 
     //Get Jokes Function
+    button.addEventListener('click', generatejoke)
 
+    generatejoke()
+
+    async function generatejoke() {
+        const res = await fetch("https://api.chucknorris.io/jokes/random")
+        const data = await res.json();
+        textNode.innerHTML = data.value
+    }
 
 
 }
